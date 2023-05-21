43d8f2519f0e01ee7f899b4f803f2c37de05efcf
Vivek <vivek9142@gmail.com>
2023-05-22 02:25:53 +0530

    added final updates

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const cors = require('cors');
-const path = require('path');
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
 
-const issueRoutes = require('./Routes/issueRoutes');
-const userRoutes = require('./Routes/userRoutes');
+const issueRoutes = require("./Routes/issueRoutes");
+const userRoutes = require("./Routes/userRoutes");
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.use('/api/v1/users/',userRoutes);
-app.use('/api/v1/issues/',issueRoutes);
+app.use("/api/v1/users/", userRoutes);
+app.use("/api/v1/issues/", issueRoutes);
 
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname,'client/build','index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,24 +1,22 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const mongoose = require('mongoose');
+dotenv.config();
 
-if(process.env.NODE_ENV === 'development'){
-    const dotenv = require('dotenv');
-    dotenv.config();
-}
+const db = process.env.DATABASE.replace("PASSWORD", process.env.PASSWORD);
 
-
-const db = process.env.DATABASE.replace('PASSWORD',process.env.PASSWORD);
-
-mongoose.connect(db,{
+mongoose
+  .connect(db, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then((con)=> {
-    console.log('connection established')
-}).catch(err => console.log(`Connection Failed Err : ${err}`));
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log("connection established");
+  })
+  .catch((err) => console.log(`Connection Failed Err : ${err}`));
 
-const app = require('./app');
+const app = require("./app");
 
-
-app.listen(process.env.PORT,()=>{
-    console.log('server is running in 3001');
+app.listen(process.env.PORT, () => {
+  console.log(`server is running in ${process.env.PORT}`);
 });
